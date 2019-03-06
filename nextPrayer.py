153fe26f87ab9954657497ac29baef560bc77a5f
* <ogadit@gmail.com>
2019-03-06 19:55:50 +0500

    prayer script

--- nextPrayer.py
+++ nextPrayer.py
@@ -5,12 +5,14 @@ import json
 import sys
 import time
 import datetime as dt
+
 try:
     tjson = requests.get("http://api.aladhan.com/v1/timingsByCity?city=Karachi&country=Pakistan&method=1&school=1").json()["data"]["timings"]
 except:
     print("Couldnt get Timings")
+    exit()
 
-ct = dt.datetime.strftime(dt.datetime.now(), "%H:%M")
+ct = dt.datetime.strptime(dt.datetime.strftime(dt.datetime.now(), "%H:%M"), "%H:%M")
 
 currentprayer = ""
 ind = 0
@@ -18,9 +20,19 @@ ind = 0
 prayers= [
         ("Fajr", tjson["Fajr"]),
         ("Dhuhr", tjson["Dhuhr"]),
-        ("Asr", tsjon["Asr"]),
+        ("Asr", tjson["Asr"]),
         ("Maghrib", tjson["Maghrib"]),
         ("Isha", tjson["Isha"])
         ]
-for x in len(prayers):
-    t = dt.datetime.strp
+for x in range(len(prayers)):
+    t = dt.datetime.strptime(prayers[x][1], "%H:%M")
+    if ct > t:
+        currentprayer = prayers[x][0]
+        ind = x
+if ind is 4:
+    ind = 0
+else:
+    ind += 1
+
+print(currentprayer)
+print(prayers[ind][0])
