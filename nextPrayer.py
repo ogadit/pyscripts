0d2bb6f9ee67e8c1231e09111976dde5d70fef64
* <ogadit@gmail.com>
2019-03-06 21:16:47 +0500

    Finishing Touches

--- nextPrayer.py
+++ nextPrayer.py
@@ -12,7 +12,7 @@ except:
     print("Couldnt get Timings")
     exit()
 
-ct = dt.datetime.strptime(dt.datetime.strftime(dt.datetime.now(), "%H:%M"), "%H:%M")
+ct = dt.datetime.strptime(dt.datetime.strftime(dt.datetime.now(), "%H:%M"), "%H:%M") #This was done to strip the datetime object of milliseconds and the date
 
 currentprayer = ""
 ind = 0
@@ -23,16 +23,18 @@ prayers= [
         ("Asr", tjson["Asr"]),
         ("Maghrib", tjson["Maghrib"]),
         ("Isha", tjson["Isha"])
-        ]
-for x in range(len(prayers)):
-    t = dt.datetime.strptime(prayers[x][1], "%H:%M")
+        ] #Make into list
+for x in range(len(prayers)): #Loop through prayer list
+    t = dt.datetime.strptime(prayers[x][1], "%H:%M") #Convert string to datetime object
     if ct > t:
         currentprayer = prayers[x][0]
-        ind = x
-if ind is 4:
-    ind = 0
-else:
-    ind += 1
+        ind = 0 if x==4 else x+1
 
-print(currentprayer)
-print(prayers[ind][0])
+
+
+nextprayer = prayers[ind][0] #Get the next prayer name
+npt = dt.datetime.strptime(prayers[ind][1], "%H:%M") #Get the next Prayers Time
+
+tt = npt - ct #Subtract The Prayer Time From Current Time
+
+print(f"It is time for {currentprayer} \nNext prayer is {prayers[ind][0]} in {tt.seconds//3600} hours and {(tt.seconds//60)%60} minutes")
